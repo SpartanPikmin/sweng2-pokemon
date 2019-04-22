47a916b7addeedd5e121377c4386e8aea0648766
unknown <michael77commins@gmail.com>
2019-04-21 21:33:24 -0700

    Added unhappy path for a pokemon created without a name

--- spec/system/add_pokemon_spec.rb
+++ spec/system/add_pokemon_spec.rb
@@ -18,3 +18,12 @@ RSpec.describe "creating a pokemon", type: :system do
 		expect(page).to have_content("Bulbasaur")
 	end
 end
+
+RSpec.describe "creating a pokemon without a name", type: :system do
+	it "allows a user to create a new pokemon and give it moves" do
+		visit new_pokemon_path
+		fill_in "Move_1", with: "Tackle"
+		click_on("Create Pokemon")
+		expect(page).not_to have_content("Bulbasaur")
+	end
+end
