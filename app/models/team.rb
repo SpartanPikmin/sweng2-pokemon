7d907149bf8cf60a529464f0f9f8b2daf2c1a559
Ethan Jones <joneetha@oregonstate.edu>
2019-04-24 19:04:24 -0700

    Normalize indentation

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,12 +1,12 @@
 class Team < ApplicationRecord
-    has_many :pokemon, dependent: :destroy
-    validates :name, presence: true
+  has_many :pokemon, dependent: :destroy
+  validates :name, presence: true
 
-    def can_fight?
-        pokemon.length > 0
-    end
+  def can_fight?
+    pokemon.length > 0
+  end
 
-    def add(new_pokemon)
-        pokemon << new_pokemon
-    end
+  def add(new_pokemon)
+    pokemon << new_pokemon
+  end
 end
--- spec/system/add_pokemon_spec.rb
+++ spec/system/add_pokemon_spec.rb
@@ -1,20 +1,20 @@
 require "rails_helper"
 
 RSpec.describe "creating a pokemon", type: :system do
-	it "allows a user to create a new pokemon and give it moves" do
-		visit new_pokemon_path
-		fill_in "Species", with: "Bulbasaur"
-		fill_in "Move_1", with: "Tackle"
-		click_on("Create Pokemon")
-		expect(page).to have_content("Bulbasaur")
-	end
+  it "allows a user to create a new pokemon and give it moves" do
+    visit new_pokemon_path
+    fill_in "Species", with: "Bulbasaur"
+    fill_in "Move_1", with: "Tackle"
+    click_on("Create Pokemon")
+    expect(page).to have_content("Bulbasaur")
+  end
 end
 
 RSpec.describe "creating a pokemon without a name", type: :system do
-	it "allows a user to create a new pokemon and give it moves" do
-		visit new_pokemon_path
-		fill_in "Move_1", with: "Tackle"
-		click_on("Create Pokemon")
-		expect(page).not_to have_content("Bulbasaur")
-	end
+  it "allows a user to create a new pokemon and give it moves" do
+    visit new_pokemon_path
+    fill_in "Move_1", with: "Tackle"
+    click_on("Create Pokemon")
+    expect(page).not_to have_content("Bulbasaur")
+  end
 end
