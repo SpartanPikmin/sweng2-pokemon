7f2df0914bb8b25cf48b28ddd6fdbb34ffda0454
unknown <michael77commins@gmail.com>
2019-05-12 18:44:30 -0700

    Added Team factory Team factory generates Teams to be used in tests

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,45 +1,40 @@
 require "rails_helper"
 
 RSpec.describe Team do
+  let(:charmander) { FactoryBot.build(:pokemon, species: 4, primary_type: "fire")}
+  let(:bulbasaur) {FactoryBot.build(:pokemon, species: 1, primary_type: "grass", secondary_type: "poison")}
+  let(:empty) {FactoryBot.build_stubbed(:team)}
+  let(:bulbasaur_team) {FactoryBot.build_stubbed(:team, pokemon: [bulbasaur])}
   setup do
-    @bulbasaur = Pokemon.new(species: 1, primary_type: "grass", secondary_type: "poison")
-    @charmander = Pokemon.new(species: 4, primary_type: "fire")
-
-    #@team_of_two = Team.new(name: "team_of_two_name")
-    #@team_of_two.add(@bulbasaur)
-    #@team_of_two.add(@charmander)
     load "#{Rails.root}/db/seeds.rb"
   end
 
   it "can fight if it has at least one pokemon" do
-    team = Team.new
+    team = empty
     expect(team.can_fight?).to be false
-    team.add(@bulbasaur)
+    team.add(charmander)
     expect(team.can_fight?).to be true
   end
 
   it "knows if it contains a pokemon of a given species" do
-    team = Team.new
-    team.add(@bulbasaur)
-    expect(team.has_pokemon?(@bulbasaur.species)).to be true
+    team = bulbasaur_team
+    expect(team.has_pokemon?(bulbasaur.species)).to be true
   end
 
   it "responds to team_efficacy_of" do
-    team = Team.new
+    team = empty
     expect(team).to respond_to :team_efficacy_of
   end
 
   it "knows the cumulative efficacy of a particular type against it's pokemon" do
-    team = Team.new
-    team.add(@bulbasaur)
+    team = bulbasaur_team
     expect(team.team_efficacy_of("grass")).to have_efficacy_of(Effect::NOT_VERY_EFFECTIVE, 2)
-    team.add(@charmander)
+    team.add(charmander)
     expect(team.team_efficacy_of("grass")).to have_efficacy_of(Effect::NOT_VERY_EFFECTIVE, 3)
   end
 
   it "distinguishes between singly and doubly effective/ineffective types" do
-    doubly_team = Team.new
-    doubly_team.add(@bulbasaur)
+    doubly_team = bulbasaur_team
     doubly_team_hash = doubly_team.efficacy_hash
     expect(doubly_team_hash[Effect::NOT_VERY_EFFECTIVE]["grass"]).to be 0
     expect(doubly_team_hash[Effect::NOT_VERY_EFFECTIVE / 2]["grass"]).to be 1
