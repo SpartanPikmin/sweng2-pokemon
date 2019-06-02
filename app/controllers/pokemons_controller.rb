72e4d0e282f675a5b43160ab1853e5f9abd6b01b
Devon Crane <cranede@10-116-10-104.wireless.oregonstate.edu>
2019-06-02 10:15:55 -0700

    fixed issue where moves2-4 are not being stored in the database

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -10,6 +10,9 @@ class PokemonsController < ApplicationController
 	  primary_type: params[:pokemon][:primary_type],
     secondary_type: params[:pokemon][:secondary_type],
     move1: params[:pokemon][:move1],
+    move2: params[:pokemon][:move2],
+    move3: params[:pokemon][:move3],
+    move4: params[:pokemon][:move4],
 	)
     if @pokemon.save
       redirect_to pokemons_path
